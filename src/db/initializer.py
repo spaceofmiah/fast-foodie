52298f5547ba74761d324df04100443068f69b28
spaceofmiah <geek_200@outlook.com>
2022-10-20 22:07:37 +0100

    Initiate declarative base in initializer

--- src/db/initializer.py
+++ src/db/initializer.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import declarative_base
+from settings.dev import DATABASE_URL
 
 
 # Create Engine
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+Base = declarative_base()
+
+
+def create_connection():
+    """Returns database engine connection"""
+    return engine.connect()
+
