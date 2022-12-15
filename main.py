c2b376c4f6b6fcd5f1c3cbadb7e0adcd94f71ccd
spaceofmiah <geek_200@outlook.com>
2022-12-15 14:31:44 +0100

    Improve food listing api view

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from typing import List
 import logging
+from typing import List
 
-from fastapi import FastAPI
-from db.services import food_services
+from sqlalchemy.orm import Session
+
+from fastapi import FastAPI, Depends
+
+from db.schemas.foods import Food
+from db.initializer import get_db
+from db.services import foods
+
 
 
 app = FastAPI()
 logger = logging.getLogger(__name__)
 
-food_db: List[str] = ["German Potatoes Slice", "Gernished Peppered Snake"]
 
+@app.get("/foods", response_model=List[Food])
+def list_foods(session:Session=Depends(get_db)):
+    """Retrieve all food records"""
+    return foods.DQL.list(session=session)
 
-@app.get("/foods")
-def foods():
-    """
-    Handles request to retrieve all meals in the database
-    """
-    return food_services.DQL.retrieve_foods()
-
-
-@app.get("/foods/{id}")
-def food(id: int):
-    try:
-        return food_db[id]
-    except IndexError:
-        return {"detail": "Not Found"}
