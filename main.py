f31324067b47087c7939b6846da07bd9721c5003
spaceofmiah <geek_200@outlook.com>
2022-12-15 18:36:39 +0100

    Add create endpoint

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import Session
 
 from fastapi import FastAPI, Depends
 
-from db.schemas.foods import Food
+from db.services import foods as food_service
+from db.schemas import foods as food_schema
 from db.initializer import get_db
-from db.services import foods
 
 
 
@@ -15,8 +15,13 @@ app = FastAPI()
 logger = logging.getLogger(__name__)
 
 
-@app.get("/foods", response_model=List[Food])
+@app.get("/foods", response_model=List[food_schema.Food])
 def list_foods(session:Session=Depends(get_db)):
     """Retrieve all food records"""
-    return foods.DQL.list(session=session)
+    return food_service.list(session=session)
+
+@app.post('/create-food/', response_model=food_schema.Food)
+def create_food(food:food_schema.FoodCreate, session:Session=Depends(get_db)):
+    """Create a food instance"""
+    return food_service.create(session=session, food=food)
 
