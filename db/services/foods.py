aef0fb83a1c438296a74484c0239257fabe6592e
spaceofmiah <geek_200@outlook.com>
2022-12-15 18:36:28 +0100

    Update services to function base

--- db/services/foods.py
+++ db/services/foods.py
@@ -1,12 +1,24 @@
-from sqlalchemy.sql import select
 from sqlalchemy.orm import Session
 from db.initializer import engine
 
-from db.models.foods import Food
+from db.models import foods as food_model
+from db.schemas.foods import FoodCreate
 
 
-class DQL:
-    @staticmethod
-    def list(session: Session):
-        """Handles request to retrieve foods"""
-        return session.query(Food).all()
+
+def list(session: Session):
+    """Handles request to retrieve foods"""
+    return session.query(food_model.Food).all()
+
+def create(session:Session, food:FoodCreate):
+    db_food = food_model.Food(
+        name=food.name, 
+        description=food.description
+    )
+    session.add(db_food)
+    session.commit()
+    session.refresh(db_food)
+    return db_food
+
+
+
