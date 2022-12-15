490018f45cace5148c69bb6877cc6ac698bc94ae
spaceofmiah <geek_200@outlook.com>
2022-12-15 14:30:43 +0100

    Add function to yield an instance of SessionLocal

--- db/initializer.py
+++ db/initializer.py
@@ -10,3 +10,11 @@ engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
