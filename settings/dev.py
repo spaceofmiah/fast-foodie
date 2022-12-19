738af6f4f30d66a475549707ccfa14a95ceb6965
spaceofmiah <geek_200@outlook.com>
2022-12-19 18:19:25 +0100

    Set application base token configurations

--- settings/dev.py
+++ settings/dev.py
@@ -9,6 +9,7 @@ config = configparser.ConfigParser()
 config.read(BASE_DIR.joinpath("config.ini"))
 
 
+# Database connection url
 DATABASE_URL = (
     "postgresql+psycopg2://{username}:{password}@{host}:{port}/{db_name}".format(
         host=config["database"]["host"],
@@ -18,3 +19,20 @@ DATABASE_URL = (
         db_name=config["database"]["database_name"],
     )
 )
+
+
+# Application secret key. Should always be kept safe and not to be tracked in
+# a public repository
+SECRET_KEY = config['app']['secret_key']
+
+# Algorithm to be used in token generation
+TOKEN_ALGORITHM = "HS256"
+
+# Total token lifespan in seconds e.g 3600 = 1hr
+TOKEN_LIFE_SPAN = 3600
+
+# URL path for which to get token
+TOKEN_URL_PATH = "token"
+
+# Account model unique field
+USER_UNIQUE_FIELD = "email"
