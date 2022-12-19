716e5f09fd23fc669c15ffaee011e68c6493fae0
spaceofmiah <geek_200@outlook.com>
2022-12-19 14:11:49 +0100

    Add password utility function to verify password

--- utils/password_utils.py
+++ utils/password_utils.py
@@ -2,4 +2,7 @@ import bcrypt
 
 
 def hash_password(password):
-	return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+	return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+def verify_password(password, hashed_password):
+	return bcrypt.checkpw(password.encode('utf8'), hashed_password)
