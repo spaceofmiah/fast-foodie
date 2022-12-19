dad6bd60cee3baef4cd6f90260f75c9812fcb0d2
spaceofmiah <geek_200@outlook.com>
2022-12-19 05:45:37 +0100

    Add service to create a new user

--- db/services/users.py
+++ db/services/users.py
@@ -1,6 +1,16 @@
 from sqlalchemy.orm import Session
+
 from db.models import users
+from db.schemas import users as schema
 
 
 def db_list(session:Session):
 	return session.query(users.User).all()
+
+def db_create(session:Session, user=schema.UserCreate):
+	"""Handles creation of a user in the database"""
+	db_user = users.User(**user.dict())
+	session.add(db_user)
+	session.commit()
+	session.refresh(db_user)
+	return db_user
