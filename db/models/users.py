2e92618dc793835426428a483fb09572105d0a7d
spaceofmiah <geek_200@outlook.com>
2022-12-19 04:24:18 +0100

    Add user model

---
+++ db/models/users.py
@@ -0,0 +1,31 @@
+from sqlalchemy import (
+	Column, 
+	String, 
+	Integer, 
+	Boolean, 
+	UniqueConstraint, 
+	PrimaryKeyConstraint
+)
+
+from db.initializer import Base
+
+
+
+class User(Base):
+	"""Models a user table"""
+	__tablename__ = "users"
+	email = Column(String(225), nullable=False, unique=True)
+	id = Column(Integer, nullable=False, primary_key=True)
+	hashed_password = Column(String, nullable=False)
+	first_name = Column(String(225), nullable=False)
+	last_name = Column(String(225), nullable=False)
+	is_active = Column(Boolean, default=False)
+
+	UniqueConstraint("email", name="uq_user_email")
+	PrimaryKeyConstraint("id", name="pk_user_id")
+
+	def __repr__(self):
+		return "<User {first_name!r} {last_name!r}>".first_name(
+			first_name = self.first_name, 
+			last_name=self.last_name
+		)
