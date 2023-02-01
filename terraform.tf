9ce7bd79504b94dac039904bff631efc349cd72d
spaceofmiah <agbonzeosazuwa@gmail.com>
2023-02-01 15:03:30 +0100

    Format terraform

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region     = var.region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
